d4255d3ac1570ddf0d85bcd05676cc480f4fcbfa
sjpark89 <sjpark@inthecore.co.kr>
2024-01-16 16:39:10 +0900

    style-components globalstyle 정의

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@
 import { RouterProvider } from 'react-router-dom'
 import Router from './Router'
 
+import { createGlobalStyle } from "styled-components";
+import reset from 'styled-reset'
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+  box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: #eeeeee;
+    font-family: 'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+`;
+
 function App() {
   return<>
+    <GlobalStyles/>
     <RouterProvider router={Router} />
   </>
 
